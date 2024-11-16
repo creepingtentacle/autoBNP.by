be55abcf9cf11f322e0c245f185e759ba7a28228
creepingtentacle <llza21022003@gmail.com>
2024-11-16 12:58:07 +0300

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "your-email@example.com"; // Замените на ваш адрес электронной почты
+    $subject = "Новая заявка с сайта";
+    $body = "Имя: $name\nЭлектронная почта: $email\nСообщение:\n$message";
+
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Сообщение успешно отправлено!";
+    } else {
+        echo "Произошла ошибка при отправке сообщения.";
+    }
+}
+?>
