aefedb7b4f5b85192901ebf5bb2ff07f656b6e42
creepingtentacle <llza21022003@gmail.com>
2024-11-16 13:38:01 +0300

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,14 +1,12 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    $phone = $_POST['phone'];
 
     $to = "llza21022003@gmail.com"; // Замените на ваш адрес электронной почты
     $subject = "Новая заявка с сайта";
-    $body = "Имя: $name\nЭлектронная почта: $email\nСообщение:\n$message";
+    $body = "Телефон клиента: $phone";
 
-    $headers = "From: $email";
+    $headers = "From: no-reply@yourdomain.com";
 
     if (mail($to, $subject, $body, $headers)) {
         echo "Сообщение успешно отправлено!";
